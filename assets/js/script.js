afda15dbf1bbf60a773b56aeee3946e6e8136ad0
pr0pr09er <113778429+pr0pr09er@users.noreply.github.com>
2022-10-28 15:25:59 +0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,116 @@
+"use strict";
+// let arr = ["Привет, ", "мир", "!"];
+// console.log(arr.join(""));
+
+// let arr = ["Привет, ", "мир", "!"];
+// text = arr.join("");
+// console.log(text);
+
+// let arr = ["Привет, ", "мир", "!"];
+// arr[0] = "Пока, ";
+// console.log(arr.join(""));
+
+// arr = new Map([
+//     ["Петя", 20000],
+//     ["Коля", 21000]
+// ])
+
+// const arr = {
+//     "ru": ["красный", "синий", "зеленый"],
+//     "en": ["red", "blue", "green"]
+// };
+// console.log(arr["ru"][0]);
+
+// const arr = ["a", "b", "c"];
+// alert(arr);
+
+// const arr = ["a", "b", "c"];
+// console.log(arr.join(""));
+
+// const arr = ["a", "b", "c", "d"];
+// console.log(`${arr[0]}+${arr[1]}, ${arr[2]}+${arr[3]}`);
+
+// const arr = [2, 5, 3, 9];
+// let result = arr[0] * arr[1] + arr[2] * arr[3];
+// console.log(result);
+
+// var obj = {'a': 1, 'b': 2, 'c': 3};
+// console.log(obj['c']);
+// console.log(obj.c);
+
+// var obj = {Коля: "1000", Вася: "500", Петя: "200"};
+// console.log(obj.Коля);
+// console.log(obj.Петя);
+
+// const daysOfWeek = {1: "Monday",
+//                     2: "Tuesday",
+//                     3: "Wednesday",
+//                     4: "Thurdsday",
+//                     5: "Friday",
+//                     6: "Saturday",
+//                     7: "Sunday"};
+// let day = new Date().getDay();
+// console.log(daysOfWeek[day]);
+
+// const daysOfWeek = {1: "Monday",
+//                     2: "Tuesday",
+//                     3: "Wednesday",
+//                     4: "Thurdsday",
+//                     5: "Friday",
+//                     6: "Saturday",
+//                     7: "Sunday"};
+// let day = 3;
+// console.log(daysOfWeek[day]);
+
+// const arr = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+// console.log(arr[1][0]);
+
+// const arr = {js: ['jQuery', 'Angilar'], php: 'hello', css: 'world'};
+// console.log(arr.js[0]);
+
+// const lang = {
+//   ru: [
+//     "Понедельник",
+//     "вторник",
+//     "среда",
+//     "четверг",
+//     "пятница",
+//     "суббота",
+//     "воскресенье",
+//   ],
+//   en: [
+//     "Monday",
+//     "Tuesday",
+//     "Wednesday",
+//     "Thurdsday",
+//     "Friday",
+//     "Saturday",
+//     "Sunday",
+//   ],
+// };
+// console.log(lang.ru[0]);
+// console.log(lang.en[2]);
+
+// const arr = {
+//     ru: [
+//       "Понедельник",
+//       "вторник",
+//       "среда",
+//       "четверг",
+//       "пятница",
+//       "суббота",
+//       "воскресенье",
+//     ],
+//     en: [
+//       "Monday",
+//       "Tuesday",
+//       "Wednesday",
+//       "Thurdsday",
+//       "Friday",
+//       "Saturday",
+//       "Sunday",
+//     ],
+//   };
+// let lang = 'ru';
+// let day = 3;
+// console.log(arr[lang][day - 1]);
